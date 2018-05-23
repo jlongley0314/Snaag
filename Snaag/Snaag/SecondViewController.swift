7ed0b705387d4b165c0ae768cbd8b34219962610
-- <jlongley0314@gmail.com>
2018-05-23 15:35:45 -0500

    setup initial Xcode Project

---
+++ Snaag/Snaag/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Snaag
+//
+//  Created by Jaremy Longley on 5/23/18.
+//  Copyright © 2018 Jaremy Longley. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
